cc7661ee2aa40b26a897cc627d0b65a7cbe9c8a6
Brlopa <brunoweidner@bluewin.ch>
2026-04-24 23:05:57 +0200

    fix api calling

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -31,4 +31,24 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", router);
 
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+
+// Get dirname compatible with esm and esbuild
+const _filename = typeof __filename !== "undefined" ? __filename : fileURLToPath(import.meta.url);
+const _dirname = typeof __dirname !== "undefined" ? __dirname : path.dirname(_filename);
+
+const frontendPath = path.resolve(_dirname, "../../sbb-connections/dist/public");
+if (fs.existsSync(frontendPath)) {
+  app.use(express.static(frontendPath));
+  app.get("*", (req, res, next) => {
+    if (!req.path.startsWith("/api")) {
+      res.sendFile(path.resolve(frontendPath, "index.html"));
+    } else {
+      next();
+    }
+  });
+}
+
 export default app;
